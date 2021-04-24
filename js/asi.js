f1bc67058e4812dd38f71fd8baadf9f98e09a7c2
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-24 20:48:46 +0530

    copy/paste or asi

---
+++ js/asi.js
@@ -0,0 +1,14 @@
+var a;
+console.log(a)
+// ASI : automatic semicolon insertion 
+// What are the rules for JavaScript's automatic semicolon insertion (ASI)? Is semicolon optional in javascript?  
+// First of all, you should know which statements are affected by the automatic semicolon insertion
+//  (also known as ASI for brevity):
+// empty statement
+// var statement
+// expression statement
+// do-while statement
+// continue statement
+// break statement
+// return statement
+// throw statement
