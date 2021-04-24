39008224141e69defce0f92a1c24df7023d75c9f
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-24 16:41:53 +0530

    expontational letaral

---
+++ js/exponential.js
@@ -0,0 +1,9 @@
+// Exponential :-  it is following format beN
+// b is base number 
+// e is serves as separetor or Exponential
+//  n is power number
+  function expontionalNum(){
+      return 7e2;      //(7 * 100)
+
+  }
+  console.log( expontionalNum());
