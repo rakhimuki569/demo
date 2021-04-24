7db2e231202011dce98c95f7429bcfcd0d87cfdd
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-24 16:54:08 +0530

    for in and for of loop diffrence

---
+++ js/forin-of.js
@@ -0,0 +1,17 @@
+let names = ['Muki','sharma','technical']
+// simple for loop but it is time taken 
+// for(let i =0; i< names.length; i++){
+//     console.log(names[i]);
+// }
+// for in loop is return only index of element
+
+// for(let ele in names){
+//     console.log(ele);
+// }
+
+// for of loop is return only element
+
+for(let ele of names){
+        console.log(ele);
+   }
+    
