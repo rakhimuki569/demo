630e53892ca41aaa4e034e736cd06982b1ebefa1
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-24 16:32:51 +0530

    null and undefined differ

---
+++ js/undefinedvsnull.js
@@ -0,0 +1,15 @@
+// 1:what is undefined and null means in javascript?
+// ans:- in js, undefined means a varable has been declared but has not yet been assigned a value
+//       null is assignment value. it can be assigned to variable as representation of no value
+// 2:show the pratical example of undefinied and null
+// 3:what is the data typeof undefined and null
+// 4:what will be the result undefined == null and undefined ===null
+// 5:can we assign the value undefinied and null
+let x;
+let y= null;
+console.log(x);
+console.log(y);
+console.log(typeof x);
+console.log(typeof y);
+console.log(undefined==null);
+console.log(undefined===null);
