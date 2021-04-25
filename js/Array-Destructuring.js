e9a0008327b6898362f54ddd685a6eef61c37f0d
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-25 18:46:11 +0530

    destructuring

---
+++ js/Array-Destructuring.js
@@ -0,0 +1,8 @@
+//  ES6 Array Destructuring explained.
+//   The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays,
+//    or properties from objects,  into distinct variables
+"using struct";
+
+let arr = [2,33.77,99];
+[num1,num2,num3,num4]=arr;
+console.log(num1);
---
+++ js/swap.js
@@ -0,0 +1,8 @@
+// write a Program to Swap Two Numbers without using Third Variable in JavaScript? 
+// How to swap two numbers without using a temporary variable?  
+
+let a= 10;
+let b=20;
+console.log(`a is ${a} and b is ${b}`);
+[b,a]=[a,b];
+console.log(`a is ${a} and b is ${b}`);
