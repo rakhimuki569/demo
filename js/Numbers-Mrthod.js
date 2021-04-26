697f5e8d532c99bb5371ab927e8a45c206b64d6d
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-26 09:06:02 +0530

    number system

---
+++ js/Numbers-Mrthod.js
@@ -0,0 +1,12 @@
+// what is ES6 New Number Methods in JavaScript? What is ES6 New Global Methods in JavaScript?  
+// ES6 New Number Methods in JavaScript 
+// ES6 added 2 new methods to the Number object:
+// Number.isInteger()
+// Number.isSafeInteger()
+
+// New Global Methods
+// ES6 also added 2 new global number methods
+// isFinite()
+// isNaN()
+  let num= 2.3;
+  console.log(isFinite(num));
