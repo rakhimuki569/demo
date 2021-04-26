05e6b57882b1fb1d9cad48631a4cb571882dfb01
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-26 08:46:43 +0530

    math operator

---
+++ js/Math-Object.js
@@ -0,0 +1,10 @@
+// ECMAScript Math methods.
+//  The math object provides you with properties and methods for mathematical constants and functions. 
+//  Unlike other global objects, Math is not a constructor.
+//   All the properties and methods of Math are static and can be called by using Math as an object without creating it.
+
+ const num1=-5.55;
+ console.log(Math.sign(num1));
+ console.log(Math.trunc(num1));
+ console.log(Math.floor(num1));
+
