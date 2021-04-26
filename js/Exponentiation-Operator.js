02757c46899841016d7a4b1ec3c22d1ead04646f
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-26 08:59:54 +0530

    exponentiation operator

---
+++ js/Exponentiation-Operator.js
@@ -0,0 +1,9 @@
+// what is Exponentiation Operator in ES6?
+// JavaScript Exponentiation Operator explained. 
+// The exponentiation operator (**) raises the first operand to the power of the second operand.
+// The Exponentiation Operator is one of ECMAscript 2016’s newest features. 
+// In order to be compatible with all browsers, etc, 
+// the Babel transpiler transforms this binary arithmetic operator into a Math.pow() method when passed through it. 
+// In this case as well as in the pre-ES2016 method, the left operand becomes the base and the right becomes the exponent.
+// The double asterisk is said to have a “tighter bind” than the single asterisk.
+console.log(3**2);
