a15a2c5c83531232d5e470a2adbfeebc6fbb1ec6
rakhimuki569 <rakhimuki569@gmail.com>
2021-04-26 18:14:28 +0530

    hosting.js clear

---
+++ js/hosting.js
@@ -0,0 +1,10 @@
+console.log("i m a " +job);
+var job="web developer";
+console.log("i m a " +job);
+ function whoami(){
+    console.log("i m a " +job);
+    var job="full stack"
+    console.log("i m a " +job); 
+ }
+ whoami();
+ 
