656b1cca36f9fc73470e9186e29ff746fd0d0c1f
rakhimuki569 <rakhimuki569@gmail.com>
2021-05-16 11:07:19 +0530

    variable declare in TS

---
+++ typescript/VariableDeclaration/type.js
@@ -0,0 +1,7 @@
+function me() {
+    for (var i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    console.log(" ok last " + i);
+}
+me();
---
+++ typescript/VariableDeclaration/type.ts
@@ -0,0 +1,10 @@
+function me(){
+
+    for(var i=0;i<10;i++ ){
+       console.log(i);
+       
+    }
+    console.log(" ok last "+i);
+    
+}
+me();
