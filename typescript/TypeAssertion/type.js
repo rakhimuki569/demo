3c117e3114725dcd517bc0a80bfdc47190a0829c
rakhimuki569 <rakhimuki569@gmail.com>
2021-05-16 12:19:27 +0530

    type assertion in TS

---
+++ typescript/TypeAssertion/type.js
@@ -0,0 +1,6 @@
+var naam;
+naam = "muki";
+var num = naam.length;
+var num1 = naam.length;
+console.log(num);
+console.log(num1);
---
+++ typescript/TypeAssertion/type.ts
@@ -0,0 +1,7 @@
+ let naam;
+ naam="muki";
+ let num= (<string>naam).length;
+let num1= (naam as string).length;
+console.log(num);
+console.log(num1);
+
